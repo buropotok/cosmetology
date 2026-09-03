2d9dde4cb70437c6d02b55202085aa14aa5bcc24
buropotok <buropotok@gmail.com>
2026-09-03 20:57:19 +0300

    Restore settings onboarding implementation

--- miniapp/settings.js
+++ miniapp/settings.js
@@ -1,1 +1,3 @@
-PLACEHOLDER
+(()=>{
+const tg=window.Telegram?.WebApp,$=s=>document.querySelector(s),settings=$('#settings-screen'),composer=$('#composer-screen'),groupScreen=$('#tg-group-screen');let managedBot=null,vkGroup=null,lastState=null;
+})();
