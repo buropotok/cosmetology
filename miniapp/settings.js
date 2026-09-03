7a74bd6979c1027d6215defdf00a7a1ca819f87f
buropotok <buropotok@gmail.com>
2026-09-03 20:57:26 +0300

    Restore full onboarding settings after accidental overwrite

--- miniapp/settings.js
+++ miniapp/settings.js
@@ -1,3 +1,1 @@
-(()=>{
-const tg=window.Telegram?.WebApp,$=s=>document.querySelector(s),settings=$('#settings-screen'),composer=$('#composer-screen'),groupScreen=$('#tg-group-screen');let managedBot=null,vkGroup=null,lastState=null;
-})();
+PLACEHOLDER
