06384385763ab2fb8310086bba400d2e8532f4aa
buropotok <buropotok@gmail.com>
2026-09-07 11:07:42 +0300

    Remove capture-phase new post lifecycle handler

--- miniapp/new-post-lifecycle.js
+++
@@ -1,7 +0,0 @@
-(()=>{
-  document.addEventListener('click',event=>{
-    const target=event.target.closest?.('#flow-new,#flow-manual');
-    if(!target)return;
-    window.dispatchEvent(new CustomEvent('cosmo-new-post',{detail:{source:target.id}}));
-  },true);
-})();
