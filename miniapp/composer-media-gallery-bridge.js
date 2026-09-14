3c9f43a9a2baecb20746bdd20a1fd5110545590e
buropotok <buropotok@gmail.com>
2026-09-14 09:28:44 +0300

    Make gallery bridge cleanup reversible

--- miniapp/composer-media-gallery-bridge.js
+++ miniapp/composer-media-gallery-bridge.js
@@ -5,6 +5,7 @@ export function initComposerMediaGalleryBridge(){
  const actions=composer?.querySelector('.composer-image-actions');
  const addDevice=actions?.querySelector('#composer-add-photo');
  if(!composer||!actions||!addDevice||actions.querySelector('#composer-media-gallery'))return()=>{};
+ const previousLabel=addDevice.textContent;
  addDevice.textContent='Добавить фото с устройства';
  const gallery=document.createElement('button');
  gallery.type='button';
@@ -12,5 +13,6 @@ export function initComposerMediaGalleryBridge(){
  gallery.textContent='Галерея Cosmo Sofa';
  addDevice.insertAdjacentElement('afterend',gallery);
  actions.classList.add('composer-image-actions--gallery');
- return initComposerMediaGallery({trigger:gallery,onAdd:file=>window.CosmoComposerImages?.addFiles?.([file])});
+ const disposeGallery=initComposerMediaGallery({trigger:gallery,onAdd:file=>window.CosmoComposerImages?.addFiles?.([file])});
+ return()=>{disposeGallery();gallery.remove();actions.classList.remove('composer-image-actions--gallery');addDevice.textContent=previousLabel};
 }
